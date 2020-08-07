439236a21bccdf6349a7d865b9543f64d8c91716
daleinen7 <daleinen@gmail.com>
2020-08-07 00:00:02 -0500

    2 player game, no winning logic, boxes don't color to player yet

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*----- constants -----*/
 const player1Color = 'red'; //orange
-const player2Color = 'turquoise';
+const player2Color = 'green';
 
 /*----- app's state (variables) -----*/
 
@@ -32,6 +32,7 @@ const lines = [
     [9,10],
     [10,11],
     [11,12],
+    [12],
     [9],
     [10],
     [11],
@@ -40,11 +41,11 @@ const lines = [
 
 let selectedLines;
 let playerTurn;
+
 /*----- cached element references -----*/
 const main = document.getElementById('main');
 const lineEls = document.querySelectorAll('.line');
 const boxesEls = document.querySelectorAll('.box');
-
 /*----- event listeners -----*/
 main.addEventListener('click', clickLine);
 
@@ -59,58 +60,91 @@ function clickLine(e) {
         if (selectedLines.includes(lineID)) {
             return;
         } else {
-            // Add selected line to selectedLines array
-            selectedLines.push(lineID);
             // Run render function
-            render();
+            gameLogic(lineID);
         }
     }
+}
+
+// Handle Main Logic
+function gameLogic(lineID) {
+    const preBoxSitch = checkClosedBox().filter(box => box >= 4);
+
+    // Add selected line to selectedLines array
+    selectedLines.push(lineID);
+
+    const newBoxSitch = checkClosedBox().filter(box => box >= 4);
+    
+    // Check if a box was closed
+    // If pre line select box count is less than post line select box count current player closed a box
+    if (preBoxSitch.reduce((acc, current)=>acc+current, 0) < newBoxSitch.reduce((acc, current)=>acc+current, 0)) {
+        // Add another entry of the previous player to the playlist
+        playerTurn.push(playerTurn[playerTurn.length - 1]);
+    } else {
+        const turn = playerTurn[playerTurn.length - 1] % 2 === 0 ? 1 : 2;
+
+        playerTurn.push( turn );
+    }
+    
+    // Check if there are any more lines to fill
+    if (checkWinner()){
+        // Win logic
+    }
+    
+    render();
+}
+
+// Check for closed box
+function checkClosedBox() {
+    // loop through selected lines referencing the lines array and every time a lineID references a box, add it to a local array of grid boxes then loop through that array and if it equals 4, close the box
+
+    const boxes = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+    // loop through selected lines
+    selectedLines.forEach(function (line){
+        // add one to the boxes index of each element of the lines array at the index of line
+        let linesArray = lines[line];
+        linesArray.forEach(function (la) {
+            boxes[la]++;
+        });
+    });
+
+    return boxes;
 }
 
 // Render
 function render() {
     // console.log(lineEls);
     // Iterate through selectedLines and activate the corrosponding ID
-    selectedLines.forEach(function (line){
-        lineEls[line - 1].style.backgroundColor = player1Color;
+    selectedLines.forEach(function (line, i){
+        console.log(eval(`player${playerTurn[i]}Color`));
+        lineEls[line - 1].style.backgroundColor = eval(`player${playerTurn[i]}Color`);
     });
-    
-    
-    // Run the check for closed box function
-    checkClosedBox();
-
-    // Check for winner
-
-    // Display message
-
-}
 
 
-
-// Check for closed box
-function checkClosedBox() {
-    // loop through selected lines referencing the lines array and every time a lineID references a box, add it to a local array of grid boxes then loop through that array and if it equals 4, close the box
-
-    const boxes = [0,0,0,0,0,0,0,0,0,0,0,0];
-
-    // loop through selected lines
-    selectedLines.forEach(function (line){
-        // add one to the boxes index of each element of the lines array at the index of line
-        let linesArray = lines[line];
-        
-        linesArray.forEach(function (la) {
-            boxes[la]++;
-        });
-    });
+    // Display boxes to fill in
+    boxes = checkClosedBox();
 
     boxes.forEach(function(box, i) {
         if (box >= 4) {
             boxesEls[i - 1].style.backgroundColor = player1Color;
         }
     });
+
+    // Display message
+
 }
 
 // Check for winner
+function checkWinner() {
+    // if every line has been selected the game is over
+    if (selectedLines.length >= 31) {
+        return true;
+    } else {
+        return false;
+    }
+}  
+
 
 // Init
 function init() {
